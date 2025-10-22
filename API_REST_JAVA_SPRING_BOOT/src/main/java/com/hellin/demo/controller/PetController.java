c124c85ab98bb9e1c40378994a96f784cb83a524
ikerrios <1925184@alu.murciaeduca.es>
2025-10-22 17:29:00 +0200

    docker

--- API_REST_JAVA_SPRING_BOOT/src/main/java/com/hellin/demo/controller/PetController.java
+++ API_REST_JAVA_SPRING_BOOT/src/main/java/com/hellin/demo/controller/PetController.java
@@ -1,51 +1,57 @@
 package com.hellin.demo.controller;
 
-import java.util.List;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
-import com.hellin.demo.repository.PetRepository;
+
+import java.util.List;
 import com.hellin.demo.entity.Pet;
+import com.hellin.demo.repository.PetRepository;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @RestController
-@RequestMapping("/pets")
+@RequestMapping("/pet")
 /**
- * En este controlador se exponen todos los endpoints referentes a Pets {{@link Pet}}
+ * En este controlador se espone los enpoint referentes a Pets 
  * @version 1.0
- * @author Iker
+ * @author Iker Ríos
  */
 public class PetController {
 
+
     private PetRepository petRepository;
 
-    /*
-     * Constructor del controlador
-     * @param petRepository Repositorio para consultar en BD
-     */
     public PetController(PetRepository petRepository) {
         this.petRepository = petRepository;
-    }
+    }  
 
-    /*
-    *  Este método devuelve el listado de Pets
-    * @return List od @linl{Pet} Información de cada mascota.
+    /**
+     * Este method devuelve el listado de mascotas
+     * @return List<Pet> informacion de cada mascota
      */
-    @GetMapping("/list")
-    public List<Pet> Hello(){
-       List<Pet> listpets = petRepository.findAll();
-        return listpets;
+
+    @GetMapping ("/list")
+    public List<Pet> hello() {
+        return petRepository.findAll();
+        
     }
 
     @PostMapping("/adopt/{id}")
+    // RedirectView --> redirigir al navegador a otra URL desde un controlador.
     public Pet adopt(@PathVariable long id) {
-       Pet pet= petRepository.findById(id).get();
-       pet.setAdopt(true);
-       return petRepository.save(pet);
-    }    
+        Pet pet = petRepository.findById(id).get();
+
+        pet.setAdopt(true);
+        return petRepository.save(pet);
+        
+        
+    }
+
 }
